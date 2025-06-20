6562e999d37d1aa515a894d1d3a45c842afe9cb2
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-06-20 14:30:01 +0200

    end of try models

--- AURA-fastapi/pre_trained_models/__init__.py
+++ AURA-fastapi/pre_trained_models/__init__.py
@@ -1,3 +1,4 @@
 from .gpt2_manager import GPT2Manager
+from .facebook_mbart_manager import FacebookMBartManager
 from .medical_mt5_manager import MedicalMT5Manager
 from .mistral_manager import MistralManager
