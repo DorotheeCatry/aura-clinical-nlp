e0bfeae5ce7a543d96077e16a8181dd3ca8d690c
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-06-26 11:29:05 +0200

    change mapping numbers

--- DATA/MORE_DATA/data_generator.py
+++ DATA/MORE_DATA/data_generator.py
@@ -15,31 +15,31 @@ class DataGenerator:
         self.all_data = []
         # Ajout d'un mapping pour les noms des pathologies les plus courantes
         # 110 : hypertension artérielle
-        self.pathology_mapping[111]= "Hypertension artérielle : HTA essentielle",
-        self.pathology_mapping[112]= "Hypertension artérielle : HTA secondaire", 
-        self.pathology_mapping[113]= "Hypertension artérielle : Complications cardiovasculaires de l'HTA"
+        self.pathology_mapping[0]= "Hypertension artérielle : HTA essentielle",
+        self.pathology_mapping[1]= "Hypertension artérielle : HTA secondaire", 
+        self.pathology_mapping[2]= "Hypertension artérielle : Complications cardiovasculaires de l'HTA"
 
-        self.all_data = self.data100.generate_111()
-        self.all_data.extend(self.data100.generate_112())
-        self.all_data.extend(self.data100.generate_113())
+        self.all_data = self.data100.generate_111(0)
+        self.all_data.extend(self.data100.generate_112(1))
+        self.all_data.extend(self.data100.generate_113(2))
 
         # 120 : Diabete
-        self.pathology_mapping[121] = "Diabète de type 2" # plus fréquent donc en premier
-        self.pathology_mapping[122] = "Diabète de type 1"
+        self.pathology_mapping[3] = "Diabète de type 2" # plus fréquent donc en premier
+        self.pathology_mapping[4] = "Diabète de type 1"
 
-        self.all_data.extend(self.data100.generate_121())
-        self.all_data.extend(self.data100.generate_122())
+        self.all_data.extend(self.data100.generate_121(3))
+        self.all_data.extend(self.data100.generate_122(4))
 
         # 130 : Troubles de l'humeur et psychiques
-        self.pathology_mapping[131] = "Troubles de l'humeur et psychiques : Dépression majeure"
-        self.pathology_mapping[132] = "Troubles de l'humeur et psychiques : Anxiété généralisée"
-        self.pathology_mapping[133] = "Troubles de l'humeur et psychiques : Troubles bipolaires "
-        self.pathology_mapping[134] = "Troubles de l'humeur et psychiques : Troubles du sommeil associés"
+        self.pathology_mapping[5] = "Troubles de l'humeur et psychiques : Dépression majeure"
+        self.pathology_mapping[6] = "Troubles de l'humeur et psychiques : Anxiété généralisée"
+        self.pathology_mapping[7] = "Troubles de l'humeur et psychiques : Troubles bipolaires "
+        self.pathology_mapping[8] = "Troubles de l'humeur et psychiques : Troubles du sommeil associés"
 
-        self.all_data.extend(self.data100.generate_131())
-        self.all_data.extend(self.data100.generate_132())
-        self.all_data.extend(self.data100.generate_133())
-        self.all_data.extend(self.data100.generate_134())
+        self.all_data.extend(self.data100.generate_131(5))
+        self.all_data.extend(self.data100.generate_132(6))
+        self.all_data.extend(self.data100.generate_133(7))
+        self.all_data.extend(self.data100.generate_134(8))
 
         # Création du DataFrame
         df_medical = pd.DataFrame(self.all_data, columns=['text', 'label'])
