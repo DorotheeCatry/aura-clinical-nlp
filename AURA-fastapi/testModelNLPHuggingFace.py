eb6e554920c4aca6a06d8dec1d9ceb9118de57f7
nicolas <nicolas.cassonnet@wanadoo.fr>
2025-06-27 10:12:56 +0200

    change tests

--- AURA-fastapi/testModelNLPHuggingFace.py
+++ AURA-fastapi/testModelNLPHuggingFace.py
@@ -1,4 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import torch
+from torch.nn.functional import softmax
 
 # Charge le tokenizer et le modèle depuis Hugging Face Hub
 # tokenizer = AutoTokenizer.from_pretrained("waelbensoltana/finetuned-medical-fr")
@@ -10,18 +12,17 @@ model = AutoModelForSequenceClassification.from_pretrained("NicoCasso/finetuned-
 texts =[]
 
 texts.append("Le patient est très diabétique..")
-texts.append("je dois surveiller mon taux de sucre")
-texts.append("j'ai une tension élevée")
-texts.append("le patient a une tension élevée")
+texts.append("Le patient doit surveiller son taux de sucre")
+texts.append("J'ai des bourdonnements d'oreilles fréquents")
+texts.append("Le patient a une tension élevée")
 texts.append("Le diabète de type 2 est difficile à contrôler dans ce cas.")
 texts.append("On note une tachycardie persistante suite à une activité physique intense.")
 texts.append("La dépression est diagnostiquée depuis plusieurs semaines.")
 texts.append("Le diabète de type 2 est difficile à contrôler dans ce cas.")
 
+labels = ['hypertension artérielle', 'troubles psychiques','diabete']
 
-print( " ________________")
-print("|")
-
+print()
 for text in texts :
     # Tokenisation
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
@@ -30,9 +31,7 @@ for text in texts :
     outputs = model(**inputs)
     prediction = outputs.logits.argmax(dim=-1).item()
 
-    print(f"| Texte source : {text}")
-    print(f"| Classe prédite : {prediction}")
-    print( "|________________")
-    print("|")
-
-print()
+    probs_list = softmax(outputs.logits).squeeze().tolist()
+    print(f"Texte source : {text} ")
+    print(f"Predictions :  {probs_list[0]:.2f}   {probs_list[1]:.2f}   {probs_list[2]:.2f}   => {prediction}") 
+    print()
